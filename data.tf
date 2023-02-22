51edd33c249aaeb8670f5da04d57a3b2d8b5ce69
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-02-22 21:15:13 +0400

    local

--- data.tf
+++ data.tf
@@ -1,7 +1,11 @@
+locals {
+  cluster_name = "${var.stack}-eks"
+}
+
 data "aws_eks_cluster" "eks_cluster" {
-  name = "${var.stack}-eks"
+  name = local.cluster_name
 }
 
 data "aws_eks_cluster_auth" "eks_cluster_auth" {
-  name = "${var.stack}-eks"
+  name = local.cluster_name
 }
